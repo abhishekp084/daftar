cb6ae84e460ebf4ba64f54240d42eb5bddcbf8ba
abhishekp084 <abhishekp0084@gmail.com>
2025-01-05 11:51:29 +0530

    1

---
+++ daftardost/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-xdnisi7fn+m)8reoq5k!t@fp2!8*c+_#t9+f5b2f===qa_1!_b"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
